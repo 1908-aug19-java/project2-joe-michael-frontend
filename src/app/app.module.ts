a14a9eca345fbc1122528b58ed254fbf4059db21
Shirosaya25 <Michaelzh2009@gmail.com>
2019-09-23 10:31:42 -0400

    Navbar and Login COmponents generated - Basic routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component'
+
+const appRoutes: Routes = [
+
+  {
+    path: 'login', component: LoginComponent,
+  }
+
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(
+      
+      appRoutes,
+
+      { 
+        enableTracing: true
+      }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
