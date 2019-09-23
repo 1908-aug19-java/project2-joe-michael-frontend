97cfcc2c8c8b8bdf84816c6b058b1ec8dc0e9e22
Shirosaya25 <Michaelzh2009@gmail.com>
2019-09-23 13:28:08 -0400

    User and Status in session

--- src/app/login.service.ts
+++ src/app/login.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, Input, Output, EventEmitter } from '@angular/core';
+import { User } from './user';
 
 @Injectable({
     providedIn: 'root'
@@ -6,31 +7,76 @@ import { Injectable, Input, Output, EventEmitter } from '@angular/core';
 export class LoginService {
 
     @Output() loginEmitter: EventEmitter<boolean> = new EventEmitter();
+    @Output() userEmitter: EventEmitter<User> = new EventEmitter();
 
     constructor() { }
 
     usernameFilter: RegExp = /^[a-zA-Z0-9@.-]{1,}$/g;
     emailFilter: RegExp = /(\b[a-zA-Z0-9.-]{3,}\b)\@(\b[a-zA-Z]{1,}\b)\.(\b[a-zA-Z]{2,}\b)/g;
 
+
+    temp1: User = {
+        id: 1,
+        username: 'mjzhang2',
+        password: 'password123',
+        email: 'mjzhang2@test.com',
+        firstname: 'Michael',
+        lastname: 'Zhang',
+        level: 1
+    };
+
+    temp2: User = {
+        id: 2,
+        username: 'kechen6',
+        password: 'password123',
+        email: 'kechen6@test.com',
+        firstname: 'Kevin',
+        lastname: 'Chen',
+        level: 1
+    };
+
     logIn(username: string, password: string): number {
 
         if (username.match(this.usernameFilter) || username.match(this.emailFilter)) {
 
-            return 1;
+            if (username === this.temp1.username && password === this.temp1.password) {
+
+                this.sendUser(this.temp1);
+                return 1;
+
+            } else if (username === this.temp2.username && password === this.temp2.password) {
+
+                this.sendUser(this.temp2);
+                return 1;
+
+            } else {
+
+                return 3;
+            }
         }
 
-
-        // TODO: Add HTTP functionality after database is integrated
         return 2;
     }
 
     change(value) {
 
+        window.sessionStorage.setItem('loggedIn', value);
         this.loginEmitter.emit(value);
     }
 
     getLoginStatus() {
 
         return this.loginEmitter;
+    }
+
+    sendUser(user) {
+
+        window.sessionStorage.setItem('user', JSON.stringify(user));
+        this.userEmitter.emit(user);
+    }
+
+    getUser() {
+
+        return this.userEmitter;
     }
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginService } from '../login.service';
 import { Router } from '@angular/router';
+import { User } from '../user';
 
 @Component({
   selector: 'app-navbar',
@@ -12,15 +13,19 @@ export class NavbarComponent implements OnInit {
   constructor(private loginService: LoginService, private router: Router) { }
 
   loginSub;
-  loginStatus = false;
+  userSub;
+  loginStatus: boolean = window.sessionStorage.getItem('loggedIn') === 'true';
+  user: User = JSON.parse(window.sessionStorage.getItem('user'));
 
   ngOnInit() {
 
       this.loginSub = this.loginService.getLoginStatus().subscribe(item => this.loginStatus = item);
+      this.userSub = this.loginService.getUser().subscribe(item => this.user = item);
   }
 
   logout() {
 
+      window.sessionStorage.clear();
       this.loginService.change(false);
       this.router.navigate(['/']);
   }
